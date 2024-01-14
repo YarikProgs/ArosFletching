060a07263d46d3031eb615cfb627b886deb53d26
YarikProgs <z.yaroslav2009@gmail.com>
2024-01-14 13:28:05 +0300

    Initial commit

--- src/main/java/net/aros/afletching/ArosFletching.java
+++ src/main/java/net/aros/afletching/ArosFletching.java
@@ -1,15 +1,15 @@
-package net.aros.template;
+package net.aros.afletching;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "template";
+public class ArosFletching implements ModInitializer {
+	public static final String MOD_ID = "afletching";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Aros's fletching is initializing!");
 	}
 }
